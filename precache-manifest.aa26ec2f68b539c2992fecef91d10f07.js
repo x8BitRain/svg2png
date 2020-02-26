65d70f654d5cafd6f33cd7e1a69e94cedbfd3924
x8BitRain <dbell@btr.pm>
2020-02-26 11:19:13 +0100

    Updates

---
+++ precache-manifest.aa26ec2f68b539c2992fecef91d10f07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1e9cd291db5efffdf2be1c576c20cb",
+    "url": "/svg2png/index.html"
+  },
+  {
+    "revision": "02253dea760e8c029dd3",
+    "url": "/svg2png/static/css/main.8dca5a5a.chunk.css"
+  },
+  {
+    "revision": "dd76cd38353b91decacf",
+    "url": "/svg2png/static/js/2.4a16513a.chunk.js"
+  },
+  {
+    "revision": "ff390947f9878c8eea2ec6e7d69b26fc",
+    "url": "/svg2png/static/js/2.4a16513a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02253dea760e8c029dd3",
+    "url": "/svg2png/static/js/main.e1688026.chunk.js"
+  },
+  {
+    "revision": "bca6f1bf70e0ad97e512",
+    "url": "/svg2png/static/js/runtime-main.c52679b3.js"
+  }
+]);
